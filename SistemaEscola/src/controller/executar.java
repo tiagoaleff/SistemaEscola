c42fca31419633a6c3689b364dafe2c451367a9e
Leandro <Leandro@172.16.6.72>
2015-09-18 15:14:56 -0300

    Patas e disposição dos arquivos atualizados, Commit para fazer o material para o grupo

---
+++ SistemaEscola/src/controller/executar.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import views.loginJFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class executar {
+    
+    public static void main(String args[]){
+        
+        loginJFrame login = new loginJFrame();
+        login.setVisible(true);
+         
+    }
+
+}
---
+++ SistemaEscola/src/controller/loginController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+/**
+ * 
+ * @author Leandro Justin Vieira
+ */
+public class loginController {
+
+    public loginController( char passString[] , String user) {
+    
+        
+    
+    }
+    
+}
