7c8a54cf75cf05305dac879eeb5babc4f507898a
tiagoaleff <tiago.aleff@gmail.com>
2015-11-20 01:12:32 -0200

    Criação da classe conexao

--- SistemaEscola/src/br/sistemaescola/conexao/Conexao.java
+++ SistemaEscola/src/br/sistemaescola/conexao/Conexao.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.sistemaescola.conexao;
+
+import br.sistemaescola.exception.ExceptionEscola;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -11,4 +11,17 @@ package br.sistemaescola.conexao;
  */
 public class Conexao {
     
+    public static Connection getConexao() throws ExceptionEscola{
+        Connection conn = null;
+        try{
+            Class.forName("oNrg.hsqldb.jdbcDriver");
+            conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/agendadb", "sa", "");            
+        }catch(SQLException ex){
+            throw new ExceptionEscola("Problemas ao conectar ao Banco de Dados");
+        }catch(ClassNotFoundException e){
+            throw new ExceptionEscola("Problemas ao conectar ao Banco de Dados");
+        }
+        return conn;
+    }
 }
+
