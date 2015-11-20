71d59d3b18c0d0e6cad6627baf9e74e463253f8e
tiagoaleff <tiago.aleff@gmail.com>
2015-11-20 01:15:56 -0200

    Criação da professorDao

--- SistemaEscola/src/br/sistemaescola/dao/ProfessorDao.java
+++ SistemaEscola/src/br/sistemaescola/dao/ProfessorDao.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.sistemaescola.dao;
 
-/**
- *
- * @author Tiago Aleff
- */
 public class ProfessorDao {
     
+    public void insert(){
+        //throw new EscolaException();
+    }
+    
+    public void select(){
+        //throw new EscolaException();
+    }
+    
+    public void deleteElement(){
+        //throw new EscolaException();
+    }        
 }
