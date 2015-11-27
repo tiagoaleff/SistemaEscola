56a83ad2e66023f47515652f73159fd8aad5aee0
tiagoaleff <tiago.aleff@gmail.com>
2015-11-27 19:32:29 -0200

    Select All de Aluno pronto, integrado com a lista - sujeto a mudancas.

--- SistemaEscola/src/br/sistemaescola/list/AlunoList.java
+++ SistemaEscola/src/br/sistemaescola/list/AlunoList.java
@@ -10,6 +10,8 @@ import br.sistemaescola.dao.AlunoDao;
 import br.sistemaescola.exception.ExceptionEscola;
 import br.sistemaescola.object.Aluno;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 
@@ -17,16 +19,19 @@ import java.util.ArrayList;
  */
 public class AlunoList {
 
-    private static ArrayList<Aluno> listAluno = new ArrayList<>();
-    // private AlunoDao alunoDao = new AlunoDao();
+    private static ArrayList<Aluno> listAluno = new ArrayList<>();    
    
     public static void addAluno(Aluno aluno){ 
        listAluno.add(aluno);
     }
     
-    public static ArrayList<Aluno> getListAluno() throws ExceptionEscola{       
-        
-        listAluno = AlunoDao.selecionarTodos();
+    public static ArrayList<Aluno> getListAluno(){                               
+        try {
+            listAluno = AlunoDao.selecionarTodos();
+        } catch (ExceptionEscola ex) {
+            Logger.getLogger(AlunoList.class.getName()).log(Level.SEVERE, null, ex);
+            // implementar gravar no arquivo log
+        }
         return listAluno;
     }
     
